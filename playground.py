a9b949555404561e697c1e2f7990b28696f4d336
Kevin Jiah-Chih Liao <me@liaokev.in>
2015-04-11 14:59:37 -0400

    Added .gitignore and created working createUrl method.

--- playground.py
+++ playground.py
@@ -4,7 +4,18 @@ import urllib2
 indicoio.config.api_key = "ed10971412405df6de77333f9fab3033"
 Nyt_Api_Key = "20e826df5fe19554ac6d8d56d9708b23:2:71828824"
 
-response = urllib2.urlopen('http://api.nytimes.com/svc/search/v2/articlesearch.json?q=Climate+Change&begin_date=20000101&end_date=20150411&api-key='   + Nyt_Api_Key)
+def createNewUrl(query, begin_date, end_date, Nyt_Api_Key):
+    query = query.replace(" ", "+")
+    url =  "http://api.nytimes.com/svc/search/v2/articlesearch.json?"
+    url += "q="             + query
+    url += "&begin_date="   + begin_date
+    url += "&end_date="     + end_date
+    url += "&api-key="      + Nyt_Api_Key
+    print url
+    return url
+
+urlToRequest = createNewUrl("Global Warming", "20000101", "20150411", Nyt_Api_Key)
+response = urllib2.urlopen(urlToRequest)
 
 html = response.read()
 
