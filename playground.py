9331e25aee1c81b4a19f173dc87addf3078fd0b3
Kevin Jiah-Chih Liao <me@liaokev.in>
2015-04-11 16:05:09 -0400

    Added Json Library import.

--- playground.py
+++ playground.py
@@ -1,5 +1,6 @@
 import indicoio
 import urllib2
+import json
 
 indicoio.config.api_key = "ed10971412405df6de77333f9fab3033"
 Nyt_Api_Key = "20e826df5fe19554ac6d8d56d9708b23:2:71828824"
@@ -11,7 +12,7 @@ def createNewUrl(query, begin_date, end_date, Nyt_Api_Key):
     url += "&begin_date="   + begin_date
     url += "&end_date="     + end_date
     url += "&api-key="      + Nyt_Api_Key
-    print url
+    print "Requesting data from NYT URL: " + url
     return url
 
 urlToRequest = createNewUrl("Global Warming", "20000101", "20150411", Nyt_Api_Key)
